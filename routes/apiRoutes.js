98aa2b8ae8fc0764cb0ceac928817d08d652c8b2
mini pony <northjw1@gmail.com>
2020-12-10 17:46:34 -0500

    adding routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,49 @@
+const Workout = require("../models/workout.js")
+
+module.exports = function (app) {
+
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+  });
+
+app.post("/api/workouts", function (req, res) {
+    Workout.create({})
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log("err", err)
+        res.json(err)
+    })
+});
+
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(data => res.json(data))
+      .catch(err => {
+          console.log("err", err)
+          res.json(err)
+      })
+});
+
+
+// DashBord routes
+
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+  .then(dbWorkouts => {
+      res.json(dbWorkouts);
+  })
+  .catch(err => {
+      res.json(err);
+  });
+}); 
+}
